682397f214bfc92916f7fec5952e7f2ddc377ad2
kmfoysal <faisal.cri@gmail.com>
2026-01-08 01:43:59 +0600

    upgrade the project

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,10 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   // url:"https://refined-vitality-523539ad53.strapiapp.com",
+  url:"http://localhost:1337",
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+
 });
